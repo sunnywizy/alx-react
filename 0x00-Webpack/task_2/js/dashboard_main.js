33946e798f2ef9452b8c8c74bfc982ce4f6a2a36
sunnywizy <sundayobasi222@gmail.com>
2024-09-02 17:00:37 +0100

    I just added some html, css and Javascript files to my repository

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,23 +1,24 @@
+// interesting jquery import thanks to babel
 import $ from 'jquery';
+// import lodash
 import _ from 'lodash';
-import '../css/main.css';  // Import CSS
+// styles
+import '../css/main.css';
 
+// append elements
+$('body').append(`<div id="logo"></div>`);
+$('body').append(`<p>Holberton Dashboard</p>`);
+$('body').append(`<p>Dashboard data for the students</p>`);
+$('body').append(`<button>Click here to get started</button>`);
+$('body').append(`<p id="count"></p>`);
+$('body').append(`<p>Copyright - Holberton School</p>`);
+
+// bind lodash's debounce to the button
+$('button').on('click', _.debounce(updateCounter, 500));
+
+let count  = 0;
+// function to update the counter
 function updateCounter() {
-  let count = 0;
-  return function () {
-    count += 1;
-    $('#count').text(`${count} clicks on the button`);
-  };
+	count++;
+	$('#count').text(`${count} clicks on the button`);
 }
-
-$(document).ready(function () {
-  $('body').prepend('<div id="logo"></div>');  // Add logo at the top
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  const counter = updateCounter();
-  $('button').on('click', _.debounce(counter, 300));
-});
